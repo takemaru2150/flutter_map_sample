9d9fd16f984457749382b378f58701d2c2cc7c78
sakurai <takeshi2jp1000@gmail.com>
2022-08-04 14:02:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# flutter_map_sample
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+## 参考
+
+- [flutter_map パッケージ](https://pub.dev/packages/flutter_map)
+- [flutter_map ドキュメント](https://docs.fleaflet.dev/)
+- [OpenStreetMap](https://openstreetmap.jp/)
+- [Mapboxを使用する](https://docs.fleaflet.dev/tile-servers/using-mapbox)
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map/plugin_api.dart';
+import 'package:latlong2/latlong.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Map Sample',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Map Sample'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: FlutterMap(
+          options: MapOptions(
+            center: LatLng(35.658034,139.701636),
+            zoom: 14,
+          ),
+          layers: [
+            TileLayerOptions(
+              urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+              userAgentPackageName: 'com.example.app',
+            ),
+            MarkerLayerOptions(
+              markers: [
+                Marker(
+                  point: LatLng(35.658034,139.701636),
+                  builder: (ctx) => const Icon(
+                    Icons.location_pin,
+                    color: Colors.redAccent,
+                  ),
+                ),
+              ],
+            ),
+          ],
+          nonRotatedChildren: [
+            AttributionWidget.defaultWidget(
+              source: 'OpenStreetMap contributors',
+              onSourceTapped: null,
+            ),
+          ],
+        )
+    );
+  }
+}
